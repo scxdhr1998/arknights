d8d2beb5f52bd8d70a6e84a9b02d47b236fa6bc0
宋崇啸 <72733583+scxdhr1998@users.noreply.github.com>
2020-12-08 01:05:52 +0800

    Add files via upload

---
+++ src/main/java/com/arkights/ArkightsApplication.java
@@ -0,0 +1,13 @@
+package com.arkights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArkightsApplication.class, args);
+	}
+
+}
