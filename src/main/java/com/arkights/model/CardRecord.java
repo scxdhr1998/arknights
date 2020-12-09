2a10a8bc8f26b47a1e969ccda23845564b3d669a
scx <scx1998@aliyun.com>
2020-12-09 18:33:03 +0800

    添加构造器

--- src/main/java/com/arkights/model/CardRecord.java
+++ src/main/java/com/arkights/model/CardRecord.java
@@ -1,5 +1,11 @@
 package com.arkights.model;
 
 public class CardRecord {
+    private String name;
+    private String gender;
 
+    public CardRecord(String name, String gender) {
+        this.name = name;
+        this.gender = gender;
+    }
 }
